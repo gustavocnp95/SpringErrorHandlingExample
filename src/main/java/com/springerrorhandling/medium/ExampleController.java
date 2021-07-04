5549c2f560d2eefda0d230a43a2d997f32e897a7
gustavocnp95 <gustavocnavarro@gmail.com>
2021-07-04 15:12:30 -0300

    feature: create specific exception to be throw and the handler

--- src/main/java/com/springerrorhandling/medium/ExampleController.java
+++ src/main/java/com/springerrorhandling/medium/ExampleController.java
@@ -3,9 +3,11 @@ package com.springerrorhandling.medium;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/example")
+@RestController
+@RequestMapping(value = "/example")
 public class ExampleController {
     @NotNull
     private final ExampleService service;
---
+++ src/main/java/com/springerrorhandling/medium/errorhandling/ApiError.java
@@ -0,0 +1,17 @@
+package com.springerrorhandling.medium.errorhandling;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+@Builder(toBuilder = true, setterPrefix = "with")
+@AllArgsConstructor
+@Data
+public final class ApiError {
+    private final int statusCode;
+    @NotNull
+    private final String message;
+    @NotNull
+    private final String debugMessage;
+}
